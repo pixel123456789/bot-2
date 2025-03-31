10e2625a717f862b91b25b2d21917c43d11f5348
pixel123456789 <92704208+pixel123456789@users.noreply.github.com>
2025-03-31 15:14:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 // The URL you want to keep alive
-const targetUrl = 'https://bot1-3bn8.onrender.com'; // Replace with the URL of the page you want to keep alive
+const targetUrl = 'https://bot-2-939z.onrender.com'; // Replace with the URL of the page you want to keep alive
 
 // Function to make an HTTP request to the target URL
 async function keepAlive() {
